13cf232709f853e2bedd3e7249126bc37f96cd97
osasusan <107266592+osasusan@users.noreply.github.com>
2022-11-30 07:38:46 +0100

    rediseño logica

--- Practica1erTrimestre/Game.swift
+++ Practica1erTrimestre/Game.swift
@@ -13,10 +13,10 @@ class Game : UIViewController {
     var correctAnswer = 0
     var life  = 0
     var vidas = 0
-    
+    var timeLeft = 0
     var points  = 0
-    var copia : [UIImage?] = []
-    var correcion : [UIImage?] = []
+    var imageInArray : [UIImage?] = []
+    var playerImageArray : [UIImage?] = []
     let digimon = [UIImage(named: "agumon"),UIImage(named:  "gatomon"),UIImage(named:  "guilmon"),UIImage(named: "veemon")]
     
     @IBOutlet weak var tvLife: UILabel!
@@ -33,203 +33,99 @@ class Game : UIViewController {
     @IBOutlet weak var tvpreba: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
+        ivDigimon.image = UIImage(named: "calendario")
         tvpreba.text = difucultad
         btRepit.tintColor = UIColor.gray
         tvPoints.text = "Puntos: \(points.description)"
         levelSelec()
+       
+      
+        
         
     }
     @IBAction func addAgumon(_ sender: Any) {
-        correcion.append(UIImage(named: "agumon"))
+        playerImageArray.append(UIImage(named: "agumon"))
         checkArray()
     }
     @IBAction func addGatomon(_ sender: Any) {
-        correcion.append(UIImage(named: "gatomon"))
-       checkArray()
+        playerImageArray.append(UIImage(named: "gatomon"))
+        checkArray()
     }
     @IBAction func addGuilmon(_ sender: Any) {
-        correcion.append(UIImage(named: "guilmon"))
-       checkArray()
+        playerImageArray.append(UIImage(named: "guilmon"))
+        checkArray()
     }
     @IBAction func addVeemon(_ sender: Any) {
-        correcion.append(UIImage(named: "veemon"))
+        playerImageArray.append(UIImage(named: "veemon"))
         checkArray()
     }
     @IBAction func repit(_ sender: Any) {
         levelSelec()
     }
     func ezGame(){
-        enabledButton()
+       
         btRepit.isEnabled = false
-        imageNil()
-        DispatchQueue.main.async{
-            sleep(3)
-            self.changeImage()
-            DispatchQueue.main.async {
-                sleep(2)
-                self.imageNil()
-                DispatchQueue.main.async {
-                    sleep(3)
-                    self.changeImage()
-                    DispatchQueue.main.async {
-                        sleep(2)
-                        self.imageNil()
-                        DispatchQueue.main.async {
-                            sleep(3)
-                            self.changeImage()
-                            DispatchQueue.main.async {
-                                sleep(2)
-                                self.imageNil()
-                                DispatchQueue.main.async {
-                                    sleep(3)
-                                    self.changeImage()
-                                    self.abledButton()
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        changeImage(time: 0.75)
+        changeImage(time: 1.5)
+        changeImage(time: 2.25)
+        changeImage(time: 3)
+       
     }
     func normalGame(){
-        enabledButton()
+       
         btRepit.isEnabled = false
-        imageNil()
-        DispatchQueue.main.async{
-            sleep(3)
-            self.changeImage()
-            DispatchQueue.main.async {
-                sleep(2)
-                self.imageNil()
-                DispatchQueue.main.async {
-                    sleep(3)
-                    self.changeImage()
-                    DispatchQueue.main.async {
-                        sleep(2)
-                        self.imageNil()
-                        DispatchQueue.main.async {
-                            sleep(3)
-                            self.changeImage()
-                            DispatchQueue.main.async {
-                                sleep(2)
-                                self.imageNil()
-                                DispatchQueue.main.async {
-                                    sleep(3)
-                                    self.changeImage()
-                                    DispatchQueue.main.async {
-                                        sleep(2)
-                                        self.imageNil()
-                                        DispatchQueue.main.async {
-                                            sleep(3)
-                                            self.changeImage()
-                                            self.abledButton()
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        changeImage(time: 0.75)
+        changeImage(time: 1.5)
+        changeImage(time: 2.25)
+        changeImage(time: 3)
+        changeImage(time: 3.75)
+        
     }
     func difGame(){
-        enabledButton()
+       
         btRepit.isEnabled = false
-        imageNil()
-        DispatchQueue.main.async{
-            sleep(3)
-            self.changeImage()
-            DispatchQueue.main.async {
-                sleep(2)
-                self.imageNil()
-                DispatchQueue.main.async {
-                    sleep(3)
-                    self.changeImage()
-                    DispatchQueue.main.async {
-                        sleep(2)
-                        self.imageNil()
-                        DispatchQueue.main.async {
-                            sleep(3)
-                            self.changeImage()
-                            DispatchQueue.main.async {
-                                sleep(2)
-                                self.imageNil()
-                                DispatchQueue.main.async {
-                                    sleep(3)
-                                    self.changeImage()
-                                    DispatchQueue.main.async {
-                                        sleep(3)
-                                        self.imageNil()
-                                        DispatchQueue.main.async {
-                                            sleep(3)
-                                            self.changeImage()
-                                            DispatchQueue.main.async {
-                                                sleep(3)
-                                                self.imageNil()
-                                                DispatchQueue.main.async {
-                                                    sleep(3)
-                                                    self.changeImage()
-                                                    self.abledButton()
-                                                    
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        changeImage(time: 0.75)
+        changeImage(time: 1.5)
+        changeImage(time: 2.25)
+        changeImage(time: 3)
+        changeImage(time: 3.75)
+        changeImage(time: 4.5)
+        
     }
     func getRandom(){
-        // let deadline = Date().advanced(by: 2)
-        //Thread.sleep(until: deadline)
         correctAnswer = Int.random(in: 0...3)
         ivDigimon.image = digimon[correctAnswer]
+        let dvimage = ivDigimon.image
+        getCopi(nImage: dvimage!)
     }
-   func changeImage(){
+    func changeImage(time : TimeInterval){
+        DispatchQueue.main.asyncAfter(deadline: .now()+time){
+            UIView.transition(with: self.ivDigimon,
+                              duration: 0.5,
+                              options: .transitionFlipFromRight,
+                              animations: {
+                self.getRandom()
+            }, completion: nil)
+        }
+    }
+    func getCopi(nImage : UIImage){
         
-        UIView.transition(with: self.ivDigimon,
-                          duration: 4.0,
-                          options: .transitionCrossDissolve,
-                          animations: {
-            self.getRandom()
-        }, completion: nil)
-        getCopi()
-    }
-    func getCopi(){
-        let nombre = ivDigimon.image!
-        copia.append(nombre)
-        let nana = copia.count
+        imageInArray.append(nImage)
+        let nana = imageInArray.count
         tvpreba.text = nana.description
-   }
-    func imageNil(){
-        UIView.transition(with: self.ivDigimon,
-                          duration: 3.0,
-                          options: .transitionCrossDissolve,
-                          animations: {
-            self.ivDigimon.image = nil
-        }, completion: nil)
     }
     func comprobation(){
-        if copia.elementsEqual(correcion){
+        if imageInArray.elementsEqual(playerImageArray){
             tvpreba.text = "correcto"
             sleep(2)
             lifeAndPointos()
-            
         }else {
             tvpreba.text = "incorrecto"
             lifeAndPointos()
         }
     }
-    func checCorretion(numer: Int ){
-        if correcion.count == numer {
+    func checkCorrection(numer: Int ){
+        if playerImageArray.count == numer {
             comprobation()
             enabledButton()
         }
@@ -247,38 +143,50 @@ class Game : UIViewController {
         btVeemon.isEnabled = true
     }
     func checkArray() {
-        tvpreba.text = correcion.count.description
+        tvpreba.text = playerImageArray.count.description
         if difucultad == "1"{
-            checCorretion(numer: 4)
+            checkCorrection(numer: 4)
         }else if difucultad == "2"{
-            checCorretion(numer: 5)
+            checkCorrection(numer: 5)
         }else {
-            checCorretion(numer: 6)
+            checkCorrection(numer: 6)
         }
     }
     func levelSelec(){
+        enabledButton()
         if difucultad == "1"{
             vidas = 5
             vidas = vidas - life
             tvLife.text = "Vidas: \(vidas.description)"
             vidas = 5
             ezGame()
-            
-            
+            let when = DispatchTime.now() + 3
+                DispatchQueue.main.asyncAfter(deadline: when){
+                    self.abledButton()
+                }
+
         }else if difucultad == "2"{
             vidas = 3
             vidas -= life
             tvLife.text = "Vidas: \(vidas.description)"
             vidas = 3
             normalGame()
-            
+            let when = DispatchTime.now() + 3.75
+                DispatchQueue.main.asyncAfter(deadline: when){
+                    self.abledButton()
+                }
+
         }else {
             vidas = 1
             vidas -= life
             tvLife.text = "Vidas: \(vidas.description)"
             vidas = 1
-            
             difGame()
+            let when = DispatchTime.now() + 4.5
+                DispatchQueue.main.asyncAfter(deadline: when){
+                    self.abledButton()
+                }
+
         }
     }
     func lifeAndPointos(){
@@ -289,17 +197,17 @@ class Game : UIViewController {
             tvPoints.text = "Puntos: \(points.description)"
             
             if points < 50{
+                imageInArray.removeAll()
+                playerImageArray.removeAll()
                 levelSelec()
-                copia.removeAll()
-                correcion.removeAll()
             }else{
                 tvpreba.text = "nivel superado"
             }
         }else if tvpreba.text == "incorrecto"{
             life += 1
             vidas -= life
-            copia.removeAll()
-            correcion.removeAll()
+            imageInArray.removeAll()
+            playerImageArray.removeAll()
             btRepit.isEnabled = true
             btRepit.tintColor = UIColor.blue
             tvLife.text = "Vidas: \(vidas.description)"
